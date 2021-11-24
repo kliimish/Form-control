fe992ba75fda225188d7fde515f1257b89a3d17f
KLMS <kaspars.klims@gmail.com>
2021-11-24 17:53:06 +0200

    done

--- js/script.js
+++ js/script.js
@@ -18,36 +18,55 @@ const showError = function (input, message) {
 };
 
 const allCool = function (input) {
-  input.classList = `all--done`;
+  const control = input.parentElement;
+  control.className = "control all--done";
 };
 
 const checkLenght = function (input, min, max) {
-  if (input.length > max) {
-    showError(`${getFieldname(input)}:`, `should be less than ${max} chars`);
-  } else if (input.length < min) {
-    showError(`${getFieldname(input)}: `, `Should be more than ${min} chars`);
+  if (input.value.length > max) {
+    showError(input, `Should be less than ${max} letters`);
+  } else if (input.value.length < min) {
+    showError(input, `Should be more than ${min} letters`);
+  } else {
+    allCool(input);
   }
 };
+
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    allCool(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+}
+
 const checkRequired = function (arr) {
   arr.forEach((input) => {
     if (input.value.trim() === "") {
       showError(input, `Field Required`);
+    } else {
+      allCool(input);
     }
   });
 };
 
 const checkPW = (pass1, pass2) => {
   if (pass1.value !== pass2.value) {
-    showError(pass2, `Passwords don't match!<br>`);
+    showError(pass2, `Passwords don't match!`);
+    showError(pass1, _);
   }
 };
 
 form.addEventListener(`submit`, function (e) {
   e.preventDefault();
+
   checkRequired([name, lastName, userName, pass1, pass2, email]);
   checkLenght(name, 3, 12);
   checkLenght(lastName, 3, 20);
   checkLenght(userName, 3, 20);
   checkLenght(pass1, 3, 15);
   checkPW(pass1, pass2);
+  checkEmail(email);
 });
